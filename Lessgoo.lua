7c7cf6b8845c8c1d445164a6a3c71deafe3ac17c
GuizzyV2 <90070644+GuizzyisbackV2LOL@users.noreply.github.com>
2024-06-07 22:29:39 -0700

    Update Lessgoo.lua

--- Lessgoo.lua
+++ Lessgoo.lua
@@ -1,3 +1,5 @@
+-- Hello if you are the owner of solara and want me to takes this down please contact me thanks!
+
 local ObscuraLua = [[ This is ObscuraLua. Secure your scripts effortlessly with ObscuraLua, your affordable Lua obfuscator. Simplifying security, one step at a time. For further details, please refer to our official website: https://obscuralua.replit.app. ObscuraLua Version - 1.0.7 ]]
 
 
